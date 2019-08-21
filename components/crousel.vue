bc122a392fef2a16288a2dd3643730f14d04bf1c
saba <ghsaba08@gmail.com>
2019-08-21 17:50:54 +0430

    news

--- components/crousel.vue
+++ components/crousel.vue
@@ -1,11 +1,68 @@
 <template>
-    $END$
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <img
+          slot="img"
+          class="d-block img-fluid w-100"
+          width="1024"
+          height="480"
+          src="https://picsum.photos/1024/480/?image=55"
+          alt="image slot"
+        >
+      </b-carousel-slide>
+    </b-carousel>
+  </div>
 </template>
 
 <script>
     export default {
-        name: "crousel"
+        name: "crousel",
+        data() {
+            return {
+                slide: 0,
+                sliding: null
+            }
+        },
+        methods: {
+            onSlideStart(slide) {
+                this.sliding = true
+            },
+            onSlideEnd(slide) {
+                this.sliding = false
+            }
+        }
     }
+
 </script>
 
 <style scoped>
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+  <div>
+    <navbar></navbar></div>
+  <div>
+  <crousel></crousel></div>
+    <div>
+        <section class="section">
+          <div class="container">
+            <div class="columns is-multiline">
+              <div
+                class="column is-one-quarter"
+                v-for="(article, index) in articles"
+                :key="index">
+                <a :href="article.url" target="_blank">
+                  <div class="card">
+                    <div class="card-image">
+                      <figure class="image is-3by2">
+                        <img :src="article.urlToImage" :alt="article.title">
+                      </figure>
+                    </div>
+                    <div class="card-content">
+                      <div class="content">{{ article.title }}</div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import navbar from '~/components/navbar.vue'
+import crousel from '~/components/crousel.vue'
+
+export default {
+  components: {
+      navbar,
+      crousel
+  },
+    async asyncData({ app }) {
+        const { articles } = await app.$axios.$get(
+            `https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=${
+                process.env.API_KEY
+            }`
+        );
+
+        return { articles };
+    },
+}
+</script>
+
+<style>
+
+</style>
